158b9bbdabb52b4a103aa0c3651ad895a862b837
Ricardo <ric.dalvarez@hotmail.com>
2023-11-16 17:22:46 +0100

    add test to get zones

--- back/src/zones/zones.controller.spec.ts
+++ back/src/zones/zones.controller.spec.ts
@@ -1,20 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ZonesController } from './zones.controller';
 import { ZonesService } from './zones.service';
+import { ZonesController } from './zones.controller';
+import { getModelToken } from '@nestjs/mongoose';
+import { Zone } from './schemas/zone.schema';
 
-describe('ZonesController', () => {
-  let controller: ZonesController;
+const MOCK_ZONES = [
+  {
+    dn: 1,
+    geometry: '44',
+  },
+];
+
+describe('ZonesService', () => {
+  let controller: ZonesService;
+  const mockZonesService = {
+    findAll: jest.fn().mockReturnValue(Promise.resolve(MOCK_ZONES)),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ZonesController],
-      providers: [ZonesService],
-    }).compile();
+      providers: [
+        ZonesService,
+        {
+          provide: getModelToken(Zone.name),
+          useValue: {},
+        },
+      ],
+    })
+      .overrideProvider(ZonesService)
+      .useValue(mockZonesService)
+      .compile();
 
-    controller = module.get<ZonesController>(ZonesController);
+    controller = module.get<ZonesService>(ZonesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should return and array of zoness ', async () => {
+    expect(await controller.findAll()).toMatchObject(MOCK_ZONES);
   });
 });
