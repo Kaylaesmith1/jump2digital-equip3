46707a431e31fe16eb26a8e35b62053bb3e9a25f
darioaladuz <baladuz98@gmail.com>
2023-11-16 23:32:15 +0100

    feat/fix-map: fix sequential batch zone call

--- front/src/components/ConcurrentAreas.jsx
+++ front/src/components/ConcurrentAreas.jsx
@@ -6,13 +6,11 @@ export default function ConcurrentAreas() {
 	const [concurrentAreas, setConcurrentAreas] = useState([]);
 
 	useEffect(() => {
-		// fetchConcurrentAreas(setConcurrentAreas);
 		const fetchPagesSequentially = async () => {
-			for (let i = 1; i < 10; i++) {
+			for (let i = 1; i <= 10; i++) {
 				if (i === 1) {
 					await fetchConcurrentAreasPage(i, 1000, setConcurrentAreas);
 				} else {
-					await new Promise((resolve) => setTimeout(resolve, 100));
 					await fetchConcurrentAreasPage(i, 2000, setConcurrentAreas);
 				}
 			}
