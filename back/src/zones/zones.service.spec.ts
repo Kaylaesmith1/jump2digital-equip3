c84ae71f1627a494b78a9554254df4b52252d322
Raul <raul.alhena@gmail.com>
2023-11-16 18:31:16 +0100

    test zones service pass

--- back/src/zones/zones.service.spec.ts
+++ back/src/zones/zones.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ZonesService } from './zones.service';
+import { Zone } from './schemas/zone.schema';
+import { getModelToken } from '@nestjs/mongoose';
+
+const MOCK_ZONES = [
+  {
+    dn: 1,
+    geometry: '44',
+  },
+];
 
 describe('ZonesService', () => {
   let service: ZonesService;
 
+  const mockZoneModel = {
+    find: jest.fn().mockReturnValue(Promise.resolve(MOCK_ZONES)),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ZonesService],
+      providers: [
+        ZonesService,
+        {
+          provide: getModelToken(Zone.name),
+          useValue: mockZoneModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ZonesService>(ZonesService);
@@ -14,5 +33,9 @@ describe('ZonesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return and array of zones', async () => {
+    expect(await service.findAll()).toMatchObject(MOCK_ZONES);
   });
 });
