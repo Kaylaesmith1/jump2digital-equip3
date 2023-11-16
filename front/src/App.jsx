d1a11106c25adc6896349b522519f76b06669436
Carlos Exojo <carlosexojogavilan@gmail.com>
2023-11-16 18:54:02 +0100

    Splash screen and routing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,14 @@
-import Map from './components/Map';
+import Map from "./components/Map";
+import SplashScreen from "./components/SplashScreen";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<>
-			<Map />
-		</>
-	);
+  return (
+    <BrowserRouter>
+      <Route path="/" element={<SplashScreen />} />
+      <Route path="/map" element={<Map />} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
