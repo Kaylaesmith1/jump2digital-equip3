51b40f4e5225a32a160df80a3988bfeab393dfe8
luciano-mc <luciano.martiin@gmail.com>
2023-11-16 17:43:27 +0100

    add navbar tags

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import Toggle from './Toggle.jsx';
 
 const Nav = () => {
+
+  const [ isChecked, setIsChecked ] = useState({
+    'Museos': { checked: true, value: 'Museos'},
+    'Actividades': { checked: true, value: 'Actividades'},
+    'Ocio': { checked: true, value: 'Ocio'},
+  });
+
+  const handleChange = (event) => {
+    setIsChecked({
+      ...isChecked,
+      [event.target.id]: {
+        checked: event.target.checked,
+        value: event.target.value
+      }
+    });
+  };
+
   return (
-    <nav className="bg-400-blue h-10 w-10">
-      Nav
+    <nav className="h-14 w-max flex items-center ml-auto px-24 gap-5">
+      <Toggle id="Museos" onChange={handleChange} checked={isChecked['Museos']?.checked || false}/>
+      <Toggle id="Actividades" onChange={handleChange} checked={isChecked['Actividades']?.checked || false}/>
+      <Toggle id="Ocio" onChange={handleChange} checked={isChecked['Ocio']?.checked || false}/>
     </nav>
   );
 }
