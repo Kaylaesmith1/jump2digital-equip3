707b9e3bf97c6a0bb73595ac3525a7bb1cbd5afd
darioaladuz <baladuz98@gmail.com>
2023-11-16 14:06:39 +0100

    feat/map: create mock museum data + fetch function

--- front/src/lib/utils.js
+++ front/src/lib/utils.js
@@ -1,9 +1,48 @@
 const mockMuseumData = [
 	{
-		lat: '',
-		lng: '',
+		latitud: 41.3784,
+		longitud: 2.1925,
 		id: 0,
-		name: ''
+		nom: "Museu d'Art Contemporani de Barcelona",
+		ambit: 'Art',
+		tipus_us: 'Públic',
+		tipus_equipament: 'Museu'
+	},
+	{
+		latitud: 41.3851,
+		longitud: 2.1734,
+		id: 1,
+		nom: "Museu Nacional d'Art de Catalunya",
+		ambit: 'Art',
+		tipus_us: 'Públic',
+		tipus_equipament: 'Museu'
+	},
+	{
+		latitud: 41.3879,
+		longitud: 2.1699,
+		id: 2,
+		nom: 'Museu Picasso',
+		ambit: 'Art',
+		tipus_us: 'Públic',
+		tipus_equipament: 'Museu'
+	},
+	{
+		latitud: 41.375,
+		longitud: 2.1444,
+		id: 3,
+		nom: 'CosmoCaixa Barcelona',
+		ambit: 'Science',
+		tipus_us: 'Públic',
+		tipus_equipament: 'Museu'
+	},
+	{
+		latitud: 41.3987,
+		longitud: 2.1905,
+		id: 4,
+		nom: 'Fundació Joan Miró',
+		ambit: 'Art',
+		tipus_us: 'Públic',
+		tipus_equipament: 'Museu'
 	}
 ];
 
