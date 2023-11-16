aa8dc645559d5b34384750acea7a9f49f6fc4357
Alex Orpi <orpi.alex@hotmail.com>
2023-11-16 18:52:53 +0100

    fix zones endpoint

--- back/src/zones/zones.controller.ts
+++ back/src/zones/zones.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  // Patch,
-  Param,
-  // Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ZonesService } from './zones.service';
 import { CreateZoneDto } from './dto/create-zone.dto';
 // import { UpdateZoneDto } from './dto/update-zone.dto';
@@ -29,14 +21,4 @@ export class ZonesController {
   findOne(@Param('id') id: string) {
     return this.zonesService.findOne(+id);
   }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateZoneDto: UpdateZoneDto) {
-  //   return this.zonesService.update(+id, updateZoneDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.zonesService.remove(+id);
-  // }
 }
